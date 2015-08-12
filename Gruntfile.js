06fd25cc72c1495a837a61d41c1fb67959cc7953
betatampw <betatampw@gmail.com>
2015-08-12 14:22:59 +0300

    git init

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function (grunt) {
+	'use struct';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  // Load the plugin.
+
+  // Register tasks.
+  grunt.registerTask('default', []);
+};
